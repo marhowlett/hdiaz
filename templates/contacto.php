d5bd5a408b275e43cc0f173ff977627f65c5dea8
marhowlett <marimar_r2003@hotmail.com>
2019-06-17 16:58:04 -0500

    .

---
+++ templates/contacto.php
@@ -0,0 +1,22 @@
+  <section id="contacto" class="template-section">
+
+        <div class="contenedor">
+
+
+            <div class="grid">
+
+                <div class="col">
+                      <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="Logo">
+                       <h2>Contact us now!</h2>
+                     <?php echo do_shortcode( '[caldera_form id="CF5d03dd6be3243"]' ); ?>
+                     <p> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/email 2.png" alt="Logo">contacto@dominioweb.com</p>
+
+                </div>
+                <div class="col prueba_col">
+                   
+                </div>
+
+
+            </div>
+        </div>
+    </section>
---
+++ templates/inicio.php
@@ -0,0 +1,207 @@
+<?php
+    /* Template Name: Inicio */ 
+    get_header();
+?>
+
+<section id="about" class="template-section">
+
+        
+
+
+            <div class="grid">
+
+                <div class="col fondo">
+                    <h2>About Us</h2>
+                    
+
+                </div>
+                <div class="col prueba_col">
+                   <p class="primera"><strong class="hh">H&H</strong> is an advanced Dental Clinic that specializes in <strong>implant dentistry</strong>, we provide personalized dental treatments. </p>
+ 
+<p>Returning the esthetic, function and health to your mouth to our patients from home and abroad.</p>
+                
+                 <a href="#" class="btn">More Details</a>
+               
+                </div>
+
+
+            </div>
+      
+    </section>
+    <section id="dental" class="template-section">
+
+        
+
+
+            <div class="grid">
+
+                <div class="col prueba_col">
+                    <h2 class="primera">About our <br>
+dental treatments</h2>
+                    <p>In our Dental center in the hearth of Mexico City, our dental specialist <strong>offer the same treatments, with the same quality brands and materials used in the US or Canada, for a fraction of the price.</strong></p>
+               
+                 <a href="#" class="btn">See all treatments</a>
+                
+
+                </div>
+                <div class="col fondo">
+                   
+                </div>
+
+
+            </div>
+      
+    </section>
+
+   <section id="four" class="template-section">
+
+       
+
+
+            <div class="grid">
+
+                <div class="col2 f1">
+                <h2>Dental<br>
+Implants</h2>
+                </div>
+                <div class="col2 f2">
+                <h2>All on four or <br>
+all on six</h2>
+                </div>
+               <div class="col2 f3">
+                <h2>Regeneration <br>
+& bone grafts</h2>
+                </div>
+               
+
+
+            </div>
+             <div class="grid">
+
+                <div class="col2 f4">
+                <h2>Dentures and <br>
+removable prosthesis</h2>
+                </div>
+                <div class="col2 f5">
+                <h2>Endodontics <br>
+(root canal)</h2>
+                </div>
+               <div class="col2 f6">
+                <h2>Crowns & <br>
+veneers</h2>
+                </div>
+                
+
+
+            </div>
+       
+    </section>
+    
+   <section id="reviews" class="template-section">
+   <h2>Reviews</h2>
+</section>
+
+ <section id="testimonios" class="template-section">
+   <h2>blablabla</h2>
+</section>
+<section id="reviews" class="template-section">
+  <div class="center">
+                 <a href="#" class="btn">See more reviews</a>
+                 </div>
+</section>
+
+<section id="frequently" class="template-section">
+
+            <div class="grid">
+
+                <div class="col fondo">
+                    <h2>Frequently <br> Asqued Questions </h2>
+
+                </div>
+                <div class="col prueba_col">
+                  
+                     <ol>
+
+                    <li >All of your staff is bilingual? </li>
+                    <li>what’s included in the price?  </li>
+                    <li> Do you offer any package that includes plane tickets, transport or hotel logging?</li>
+                    <li>Payment methods</li>
+                    <li>How can I set an appointment? </li>
+                    <li>Do I need to sign a contract or a consent </li>
+                    <li>The location is safe. </li>
+                    <li>How far is the airport?</li>
+                    <li>Do you attend children?</li>
+
+</ol>
+                  <h3>Do you have any question? <br>
+                  PLEASE </h3>
+                
+                 <a href="#" class="btn">More Details</a>
+               
+                </div>
+
+
+            </div>
+      
+    </section>
+    <section id="make" class="template-section">
+
+            <div class="grid">
+
+                <div class="col prueba_col">
+                    <h2>Make a resevation! <br>
+<span>In just 3 simple steps</span> </h2>
+                
+                     <ol>
+
+                    <li >Contact us, <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/email.png" alt="Iconos"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/skype.png" alt="Iconos"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/whatssap.png" alt="Iconos"></li>
+                    <li>Receive an approximated estimate for your case*</li>
+                    <li class="we"> *We can solicitate exams such as a CT scan, Xrays, to make an evealuation of your case</li>
+                    
+
+</ol>
+                  
+
+                </div>
+                <div class="col fondo">
+                  
+               
+                </div>
+
+
+            </div>
+      
+    </section>
+    <section id="card" class="template-section">
+
+            <div class="grid">
+
+                <div class="col fondo">
+                    <h2 class="how" >How can I pay?</h2>
+
+                </div>
+                <div class="col prueba_col">
+                  <h2>Payments are made <br>
+<span>in our facilities and <br>
+we accept</span></h2>
+                     <ol>
+
+                    <li ><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/dinero.png" alt="Iconos"> USD in cash </li>
+                    <li ><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/tarjeta.png" alt="Iconos"> Debit & Credit Cards (6% plus is added)  </li>
+                    <li ><img class="pay" src="<?php echo get_stylesheet_directory_uri(); ?>/img/paypal.png" alt="Iconos"> Paypal </li>
+                    
+
+</ol>
+                 
+               
+                </div>
+
+
+            </div>
+      
+    </section>
+<?php include 'contacto.php'; ?>
+
+<?php 
+    get_footer();
+?>
