f3ffaa536edbb55ee27f55dab08746d666a7e360
marhowlett <marimar_r2003@hotmail.com>
2022-06-08 12:02:30 -0500

    .

--- templates/aboutsus.php
+++ templates/aboutsus.php
@@ -138,7 +138,7 @@
                 <div class="col">
                       <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="Logo">
                        <h2>Contact us now!</h2>
-                     <?php echo do_shortcode( '[caldera_form id="CF5d03dd6be3243"]' ); ?>
+                     <?php echo do_shortcode( '[wpforms id="36"]' ); ?>
                      <p> <img class="email" src="<?php echo get_stylesheet_directory_uri(); ?>/img/email 2 copia.png" alt="Logo">contacto@dentalmexicocity.com</p>
 
                 </div>
--- templates/turismo.php
+++ templates/turismo.php
@@ -24,7 +24,7 @@ of our beautiful country</h2>
                 <div class="col">
                       <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="Logo">
                        <h2>Contact us now!</h2>
-                     <?php echo do_shortcode( '[caldera_form id="CF5d03dd6be3243"]' ); ?>
+                     <?php echo do_shortcode( '[wpforms id="36"]' ); ?>
                      <p> <img class="email" src="<?php echo get_stylesheet_directory_uri(); ?>/img/email 2 copia.png" alt="Logo">contacto@dentalmexicocity.com</p>
 
                 </div>
